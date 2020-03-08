5c5670558f82927582e39c77fe0f388f5649e13f
luminousbeam <jocelyn.jeriah@gmail.com>
2020-03-08 19:23:12 +0000

    removed unnecessary function calls

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-//  const b13 = require('./chap_1/basic_13.js')
-
-//b13.printNumsES6(10)
-
-//b13.printNumsAndSumES6(255)
-
-//b13.printMaxOfArrayES6([123,573,845,21,40,39])
-
-//b13.returnOddsArrayES6(24)
-
-//b13.returnArrayCountGreaterThanYES6([1,2,3,4,5,6,7,8,9,10,11,12], 5)
-
-//b13.maxMinAvgES6([0,1,2,3,4,5,6,7,8,9,10])
-
-//b13.swapES6([1,2,3,-4,-5,-6,7,8,9,-10], 'Dojo')
-
-//b13.printOddsES6(12)
-
-//b13.printArrValsES6([0,1,2,3,4,5,6,7,8,9,10])
-
-//b13.printAvgOfArrayES6([0,1,2,3,4,5,6,7,8,9,10])
-
-//b13.squareArrayValsES6([1,2,3,4,5])
-
-//b13.shiftLeftES6([1,2,3,4])
+const b13 = require('./chap_1/basic_13.js')
 
 const c1_1 = require('./chap_1/chap_1_1.js')
 
